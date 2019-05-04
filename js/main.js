89083fdd2ac8da868accac44a15894a515766109
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-05-04 22:31:15 +0200

    sorted array of staff members

--- js/main.js
+++ js/main.js
@@ -20,28 +20,50 @@ class staffMember {
 }
 
 // taking out functions from event listiner for code readability
+const sortByHoursWorked = (staffArray) => {
+    staffArray.sort((member1, member2) => {
+        if (member1.hoursWorked < member2.hoursWorked){
+            return 1
+        } else if (member1.hoursWorked > member2.hoursWorked){
+            return -1
+        } else {
+            return 0
+        }
+    })
+
+    return staffArray
+}
+const showBestStaffMembers = (staffArray) => {
+
+}
 
 
+let staffMembersObjects = [];
 btn.addEventListener('click', ()=> {
-    let staffMembersObjects = [];
+    
     let staffMembers = document.querySelectorAll('div[id^="pracownik"]');
 
+    // use information in div to create a object
     staffMembers.forEach((staffMemberDiv)=> {
         let staffMemberObject = new staffMember(
                 staffMemberDiv.children[0].innerText,
-                staffMemberDiv.children[1].value,
-                staffMemberDiv.children[2].value
-            );
-        staffMembersObjects.push(staffMemberObject);
+                Number(staffMemberDiv.children[1].value),
+                Number(staffMemberDiv.children[2].value)
+        );
         
         // set red for slackers
         if(staffMemberObject.isSlackingOff){
             staffMemberDiv.classList.add('slacking__staff__member')
         }
 
-        // calculate the pay
+        // calculate the pay and update the DOM with this information
         staffMemberDiv.children[3].innerText = staffMemberObject.calculatePay();
+
+        // update the array of objects with newly created object
+        staffMembersObjects.push(staffMemberObject);
     })
 
     console.log(staffMembersObjects)
 })
+
+
